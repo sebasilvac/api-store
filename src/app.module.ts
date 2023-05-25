1443c13a015e46e6f246aaf40379968cbd7b809d
Sebastián Silva <sebasilvac88@gmail.com>
2023-05-24 20:17:28 -0400

    other fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    ProductsModule,
-    CategoriesModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, ProductsModule, CategoriesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
