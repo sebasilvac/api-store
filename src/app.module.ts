d15c37d0c37ecae6c81dc7a7a0a5c90fc83548b6
Sebastián Silva <sebasilvac88@gmail.com>
2023-05-25 19:38:26 -0400

    add static index

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [UsersModule, ProductsModule, CategoriesModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    UsersModule,
+    ProductsModule,
+    CategoriesModule,
   controllers: [],
   providers: [],
 })
