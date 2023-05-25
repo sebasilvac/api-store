0bb2b1aea336522cf870a020dd598945e14cbbbe
Sebastián Silva <sebasilvac88@gmail.com>
2023-05-25 19:39:08 -0400

    add mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,20 @@ import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/store'),
     UsersModule,
     ProductsModule,
     CategoriesModule,
+    CommonModule,
+  ],
   controllers: [],
   providers: [],
 })
